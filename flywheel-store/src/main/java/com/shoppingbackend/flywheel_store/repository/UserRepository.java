66e62bfe74cfcbbd45172c12c9f473a130143035
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2025-12-09 00:48:43 +0530

    Add UserRepository

---
+++ flywheel-store/src/main/java/com/shoppingbackend/flywheel_store/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.shoppingbackend.flywheel_store.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shoppingbackend.flywheel_store.model.User;
+
+public interface UserRepository extends JpaRepository <User, Long>{
+
+    boolean existsByEmail(String email);
+
+    User findByEmail(String email);
+
+}
