bf04c5cd2ac5da70414d3c8e909d18d13d1aed5b
Kernal-Kratos <224555041+Kernel-Kratos@users.noreply.github.com>
2025-12-10 01:11:14 +0530

    Add JwtUtils

---
+++ flywheel-store/src/main/java/com/shoppingbackend/flywheel_store/security/jwt/JwtUtils.java
@@ -0,0 +1,75 @@
+package com.shoppingbackend.flywheel_store.security.jwt;
+
+
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import com.shoppingbackend.flywheel_store.security.user.ShopUserDetails;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtils {
+
+    @Value("${auth.token.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${auth.token.expirationInMils}")
+    private int expirationTime;
+
+    public String generateTokenForUser(Authentication authentication){
+        ShopUserDetails principalUser = (ShopUserDetails) authentication.getPrincipal();
+
+        List <String> roles = principalUser.getAuthorities()
+                .stream()
+                .map(GrantedAuthority :: getAuthority)
+                .toList();
+        return Jwts.builder()
+                .setSubject(principalUser.getEmail())
+                .claim("id", principalUser.getId())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime()+ expirationTime))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                 .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException
+                | IllegalArgumentException e) {
+            throw new JwtException(e.getMessage());
+        }
+    }
+}
